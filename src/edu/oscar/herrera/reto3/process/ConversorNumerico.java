29999f34f9347a4cf167b98047b1c0ffb4189f79
Oscar Herrera <105088864+Oscar-F-Herrera@users.noreply.github.com>
2023-02-07 09:25:08 -0600

    Primer commit del reto 3

---
+++ src/edu/oscar/herrera/reto3/process/ConversorNumerico.java
@@ -0,0 +1,20 @@
+package edu.oscar.herrera.reto3.process;
+
+/**
+ * Esta clase contiene metodos para convertir números de una base numérica a otra
+ */
+
+public class ConversorNumerico {
+
+    /**
+     * Este método recibe un string en forma decimal y convierte ese número a su representación binaria
+     *
+     * @param decimal El número ingresado por el usuario
+     *
+     * @return Regresa el número en formato binario
+     */
+
+    public static String convertirDecimalABinario(String decimal){
+
+    }
+}

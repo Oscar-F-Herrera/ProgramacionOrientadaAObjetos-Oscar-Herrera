c60db3229d57b809ed625fcd3881be5432cc8e9a
Oscar Herrera <105088864+Oscar-F-Herrera@users.noreply.github.com>
2023-04-22 20:05:23 -0600

    Reto 11

---
+++ src/edu/oscar/herrera/reto11/Main.java
@@ -0,0 +1,15 @@
+package edu.oscar.herrera.reto11;
+
+import edu.oscar.herrera.reto11.ui.CLI;
+
+/**
+ * El único propósito de esta clase es llamar al CLI
+ */
+public class Main {
+    /**
+     * Método main que no contiene lógica
+     */
+    public static void main(String[] args) {
+        CLI.mostrarMenu();
+    }
+}
---
+++ src/edu/oscar/herrera/reto11/ui/CLI.java
@@ -0,0 +1,75 @@
+package edu.oscar.herrera.reto11.ui;
+
+import edu.oscar.herrera.reto11.process.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * En esta clase se crean varios objetos y se imprimen los métodos
+ */
+public class CLI {
+    /**
+     * Este método crea objetos y llama a los métodos restantes
+     */
+    public static void mostrarMenu(){
+        //Guardar Inges en el archivo de texto
+
+        ArrayList<Empleado> primerLista = new ArrayList<>();
+
+        primerLista.add(new JrEngineer("A","A",29,1000000,1000000,"Python","AAAA0001"));
+        primerLista.add(new JrEngineer("B","B",27,2000000,2000000,"Java","BBBB0002"));
+        primerLista.add(new JrEngineer("C","C",29,3000000,3000000,"Javascript","CCCC0003"));
+        primerLista.add(new JrEngineer("D","D",18,4000000,4000000,"C++","DDDD0004"));
+        primerLista.add(new JrEngineer("E","E",26,5000000,5000000,"C","EEEE0005"));
+        primerLista.add(new JrEngineer("F","F",19,6000000,6000000,"C#","FFFF0006"));
+        primerLista.add(new JrEngineer("G","G",26,7000000,7000000,"PHP","GGGG0007"));
+        primerLista.add(new JrEngineer("H","H",29,8000000,8000000,"Java","HHHH0008"));
+        primerLista.add(new JrEngineer("I","I",19,9000000,9000000,"Python","IIII0009"));
+        primerLista.add(new JrEngineer("J","J",21,1234567,1234567,"C++","JJJJ0010"));
+
+        try{
+            FileWriter writer = new FileWriter("src/edu/oscar/herrera/reto11/ui/Lista.txt");
+            for (Empleado i:primerLista) {
+                writer.write(i + "\n");
+            }
+            writer.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        //Leer los Inges del Archivo de texto y guardarlos en un arraylist
+
+        ArrayList<String> segundaLista = new ArrayList<>();
+
+        Path path = Paths.get("src/edu/oscar/herrera/reto11/ui/Lista.txt");
+
+        try {
+            List<String> lineasDelArchivo = Files.readAllLines(path);
+
+            for (String line : lineasDelArchivo) {
+                segundaLista.add(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        //Imprimir inges mayores de 20
+        imprimirMayorA20(segundaLista);
+    }
+
+    public static void imprimirMayorA20(ArrayList<String> lista){
+        for (String empleado : lista) {
+            String[] atributos = empleado.split(";");
+            int edad = Integer.parseInt(atributos[3]);
+            if (edad > 20) {
+                System.out.println(empleado);
+            }
+        }
+    }
+}
